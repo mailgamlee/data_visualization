d3913991e5019184fa5d1a7f3c1c831ce71d14e1
mailgamlee <3581626452@qq.com>
2026-09-01 22:38:34 +0800

    Update CH4_19_1三维瀑布图.py

--- 三维瀑布图/CH4_19_1三维瀑布图.py
+++ 三维瀑布图/CH4_19_1三维瀑布图.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 30 10:55:50 2019
-
-@author: Jie Zhang，微信公众号【EasyShu】，本代码源自《Python数据可视化之美》
-"""
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
